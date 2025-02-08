867cbbf0360fb64a4bab474897bf21fd6fc475a1
Zunaid_Sultan <zunaidsultan786@gmail.com>
2025-02-08 18:10:21 +0500

    modify some layout

--- app/page.tsx
+++ app/page.tsx
@@ -15,18 +15,18 @@ export const metadata: Metadata = {
 export default async function Home() {
 
   const user = await currentUser()
-    // console.log(user?.privateMetadata.cards)
+  // console.log(user?.privateMetadata.cards)
 
   return (
-    <div className="container mx-auto px-4 py-4">
-      <div className="grid grid-cols-1 md:grid-cols-2 justify-between items-center gap-4">
-        <div className="space-y-8">
+    <div className="container mx-auto px-4 py-2 flex justify-center items-center min-h-screen gap-5">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-2 max-w-full w-full justify-items-center">
+        <div className="space-y-8 p-4 w-full md:w-3/4">
           <AddCard />
-          <YourCards cards = { Array.isArray(user?.privateMetadata.cards) ? user?.privateMetadata.cards: [] } />
+          <YourCards cards={Array.isArray(user?.privateMetadata.cards) ? user?.privateMetadata.cards : []} />
         </div>
-        <div className="space-y-8">
+        <div className="space-y-8 p-4 w-full md:w-3/4">
           <AddPassword />
-          <YourPasswords passwords = { Array.isArray(user?.privateMetadata.passwords) ? user?.privateMetadata.passwords: [] } />
+          <YourPasswords passwords={Array.isArray(user?.privateMetadata.passwords) ? user?.privateMetadata.passwords : []} />
         </div>
       </div>
     </div>
